227bbb0f94ad7cf1cbc8b59f8a0ec2c4cff2bb1c
Anastasia-Sterh <80683348+Anastasia-Sterh@users.noreply.github.com>
2021-04-07 12:17:47 +0300

    New Code

---
+++ Main/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticketCoast= 1000;
+int bonusMiles = 20;
+long Bonus = ticketCoast / bonusMiles;
+System.out.println(Bonus);
+}
+}
